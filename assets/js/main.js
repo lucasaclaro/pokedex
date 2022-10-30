13d0d57483cb83e89a62d05debf726e449f9c5be
Lucas Claro <lucas-claro@hotmail.com>
2022-10-30 20:53:36 -0300

    last commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,14 @@
 const pokemonNumber = document.querySelector('.number');
 const pokemonImage = document.querySelector('.image');
 const pokemonName = document.querySelector('.name');
+const pokemonType = document.querySelector('.type');
+const pokemonWeight = document.querySelector('.weight');
+const buttonPrev = document.querySelector('#iconLeft');
+const buttonNext = document.querySelector('#iconRight');
+let searchPokemon = 1;
+
+
+
 
 
 
@@ -14,8 +22,24 @@ const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
 
     pokemonNumber.innerHTML = `#${data.id}`;
-    pokemonName.innerHTML = `Name: ${data.name}`;
+    pokemonName.innerHTML = `Name: ${data.name}`.toLocaleUpperCase();
     pokemonImage.src = data['sprites']['other']['dream_world']['front_default'];
+    pokemonType.innerHTML = `Type: ${data['types'][0]['type']['name']}`.toLocaleUpperCase();
+    pokemonWeight.innerHTML = `Weight: ${data.weight}`.toLocaleUpperCase();
+    
 }
 
-renderPokemon('7');
+
+buttonPrev.addEventListener('click', () => {
+  searchPokemon -= 1;
+  renderPokemon(searchPokemon);
+});
+
+buttonNext.addEventListener('click', () => {
+  searchPokemon += 1;
+  renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
+
+
